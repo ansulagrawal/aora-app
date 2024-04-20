cbbe318345eb1adca87bc6b44bb3cfb966de4cfd
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-04-20 13:59:54 +0530

    Completed Appwrite configration and Signup configration

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
+import GlobalProvider from '../context/GlobalProvider';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -35,14 +36,14 @@ const RootLayout = () => {
   }
 
   return (
-    <>
+    <GlobalProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </GlobalProvider>
   );
 };
 
